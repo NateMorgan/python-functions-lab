05f0cdbc716fd8571ea235fbb8f3e0761924c0b3
Nate Morgan <nathanielrmorgan@gmail.com>
2022-11-01 15:10:26 -0400

    solve challenge two

--- python-functions.py
+++ python-functions.py
@@ -1,3 +1,5 @@
+import math
+
 def sum_to(n):
   output = 0
   for i in range(1,n+1):
@@ -6,3 +8,13 @@ def sum_to(n):
 
 assert sum_to(6) == 21 , "sum_to failed testing"
 assert sum_to(10) == 55 , "sum_to failed testing"
+
+def largest(list):
+  max = -math.inf
+  for i in list:
+    if i > max:
+      max = i
+  return max
+
+assert largest([1, 2, 3, 4, 0]) == 4, "largest failed testing"
+assert largest([10, 4, 2, 231, 91, 54]) == 231, "largest failed testing"
